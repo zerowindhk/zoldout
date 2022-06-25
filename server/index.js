a94f6d606dc993f55e3d9a2886dda275d83da1cc
jack.tsang <jack.tsang@gti.com.hk>
2022-06-25 11:38:24 +0800

    fix dir name

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
+console.log('directory-name 👉️', __dirname);
+// Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+// Handle GET requests to /api route
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from server!' });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
