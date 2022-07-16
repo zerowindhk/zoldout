c9b96b3c96d6608572a24ae11b203708f66369db
jack.tsang <jack.tsang@gti.com.hk>
2022-07-16 14:00:23 +0800

    change name

--- server/src/discord/commands/excel.js
+++ server/src/discord/commands/excel.js
@@ -7,7 +7,7 @@ module.exports = {
     .setDescription('提供資料來源的 Google Sheet'),
   async execute(interaction) {
     const embed = new MessageEmbed({
-      title: 'Zold:Out 武器素材查詢區',
+      title: 'Zold:Out 查詢區',
       url: 'https://docs.google.com/spreadsheets/d/1CeTO-Bae2xNGrAtTo1zAb81joirxu4CGnrdVieXQbfU/view#gid=0',
     });
     await interaction.reply({
